9dbba7c968678ae41e716702f8a21d5232606941
Enfield-Li <1076462153@qq.com>
2022-06-15 04:57:55 +0800

    Catch error

--- client/src/myAuth.ts
+++ client/src/myAuth.ts
@@ -8,26 +8,34 @@ type User = {
 // https://marmelab.com/react-admin/AuthProviderWriting.html#:~:text=%3D%20%7B%0A%20%20%20%20//%20...-,Example,-Here%20is%20a
 export const myAuth: AuthProvider = {
   login: async ({ username, password }) => {
-    const res = await fetch("http://localhost:3080/sales/login", {
-      method: "POST",
-      credentials: "include",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ saleName: username, rawPassword: password }),
-    });
-    const user: User = await res.json();
+    try {
+      const res = await fetch("http://localhost:3080/sales/login", {
+        method: "POST",
+        credentials: "include",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ saleName: username, rawPassword: password }),
+      });
+      const user: User = await res.json();
 
-    localStorage.setItem("saleName", user.fullName);
+      localStorage.setItem("saleName", user.fullName);
+    } catch (error) {
+      return Promise.reject(error);
+    }
   },
   logout: () => {
-    fetch("http://localhost:3080/sales/logout", {
-      method: "POST",
-      credentials: "include",
-    });
+    try {
+      fetch("http://localhost:3080/sales/logout", {
+        method: "POST",
+        credentials: "include",
+      });
 
-    localStorage.removeItem("saleName");
-    return Promise.resolve();
+      localStorage.removeItem("saleName");
+      return Promise.resolve();
+    } catch (error) {
+      return Promise.reject(error);
+    }
   },
   checkAuth: () =>
     localStorage.getItem("saleName") ? Promise.resolve() : Promise.reject(),
@@ -41,19 +49,23 @@ export const myAuth: AuthProvider = {
     return Promise.resolve();
   },
   getIdentity: async () => {
-    const res = await fetch("http://localhost:3080/sales/verify", {
-      method: "POST",
-      credentials: "include",
-    });
+    try {
+      const res = await fetch("http://localhost:3080/sales/verify", {
+        method: "POST",
+        credentials: "include",
+      });
 
-    if (res.ok) {
-      const user: User = await res.json();
-      localStorage.setItem("saleName", user.fullName);
-      return Promise.resolve(user);
+      if (res.ok) {
+        const user: User = await res.json();
+        localStorage.setItem("saleName", user.fullName);
+        return Promise.resolve(user);
+      }
+
+      localStorage.removeItem("saleName");
+      return Promise.reject();
+    } catch (error) {
+      return Promise.reject(error);
     }
-
-    localStorage.removeItem("saleName");
-    return Promise.reject();
   },
   getPermissions: () => Promise.resolve(""),
 };
