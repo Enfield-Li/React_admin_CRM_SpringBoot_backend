46f3d27f5ca9f1a49fb8da48bebe754f7502eea4
Enfield-Li <1076462153@qq.com>
2022-06-16 14:10:11 +0800

    Role setup

--- server/src/main/java/com/example/demo/security/SecurityConfig.java
+++ server/src/main/java/com/example/demo/security/SecurityConfig.java
@@ -1,18 +1,16 @@
-package com.example.demo;
+package com.example.demo.security;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import static com.example.demo.security.ApplicationUserRole.ADMIN;
+import static com.example.demo.security.ApplicationUserRole.STUDENT;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.User.UserBuilder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
@@ -46,10 +44,17 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
       .builder()
       .username("student1")
       .password(passwordEncoder.encode("student1"))
-      .roles("Student") // ROLE_Student
+      .roles(STUDENT.name()) // ROLE_Student
       .build();
 
-    return new InMemoryUserDetailsManager(userOne);
+    UserDetails adminOne = User
+      .builder()
+      .username("admin1")
+      .password(passwordEncoder.encode("admin1"))
+      .roles(ADMIN.name()) // ROLE_Admin
+      .build();
+
+    return new InMemoryUserDetailsManager(userOne, adminOne);
   }
   //   @Override
   //   public void configure(AuthenticationManagerBuilder authBuilder)
