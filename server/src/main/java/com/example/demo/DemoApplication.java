94d4214d151d819af18a0333e5548d3dcd5f02bb
Enfield-Li <1076462153@qq.com>
2022-06-19 04:16:45 +0800

    Open url on start

--- server/src/main/java/com/example/demo/DemoApplication.java
+++ server/src/main/java/com/example/demo/DemoApplication.java
@@ -1,12 +1,23 @@
 package com.example.demo;
 
+import java.io.IOException;
+import javax.swing.DesktopManager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
 
-  public static void main(String[] args) {
+  public static void main(String[] args) throws IOException {
     SpringApplication.run(DemoApplication.class, args);
+    // openHomePage();
+  }
+
+  private static void openHomePage() throws IOException {
+    Runtime rt = Runtime.getRuntime();
+    rt.exec(
+      "rundll32 url.dll,FileProtocolHandler " +
+      "http://localhost:3080/swagger-ui/index.html"
+    );
   }
 }
