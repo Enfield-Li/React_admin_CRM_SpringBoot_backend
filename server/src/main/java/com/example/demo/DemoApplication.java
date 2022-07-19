9af0edd8ccf2ac41b8355df79b92e3e45898a746
Enfield-Li <1076462153@qq.com>
2022-07-20 01:11:10 +0800

    Half done

--- server/src/main/java/com/example/demo/DemoApplication.java
+++ server/src/main/java/com/example/demo/DemoApplication.java
@@ -1,57 +1,15 @@
 package com.example.demo;
 
-import com.example.demo.auth.user.ApplicationUserRole;
-import com.example.demo.entity.Company;
-import com.example.demo.entity.Sale;
-import com.example.demo.repository.SaleRepository;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class DemoApplication {
 
   public static void main(String[] args) throws IOException {
-    ConfigurableApplicationContext ctx = SpringApplication.run(
-      DemoApplication.class,
-      args
-    );
+    SpringApplication.run(DemoApplication.class, args);
     // openSwaggerUI();
-
-    SaleRepository saleRepo = ctx.getBean(SaleRepository.class);
-
-    Sale sale = new Sale();
-    sale.setId(1L);
-    sale.setPassword("password");
-    sale.setLast_name("last_name");
-    sale.setFirst_name("first_name");
-    sale.setRole(ApplicationUserRole.SALE_ADMIN);
-
-    Company company1 = new Company();
-    company1.setSale(sale);
-    company1.setSize(10);
-    company1.setName("company1");
-    company1.setCity("guangzhou");
-    company1.setSector("consumer");
-    company1.setStateAbbr("guangdong");
-
-    Company company2 = new Company();
-    company2.setSale(sale);
-    company2.setSize(20);
-    company2.setName("company2");
-    company2.setCity("guangzhou");
-    company2.setSector("consumer");
-    company2.setStateAbbr("guangdong");
-
-    sale.setCompanies(Set.of(company1, company2));
-    // sale.setCompanies(Set.of(company1, company2));
-    Sale savedSales = saleRepo.save(sale);
   }
 
   private static void openSwaggerUI() throws IOException {
--- server/src/test/java/com/example/demo/mapper/companyMapperTest.java
+++ server/src/test/java/com/example/demo/mapper/companyMapperTest.java
@@ -43,32 +43,54 @@ public class companyMapperTest implements WithAssertions {
 
   @BeforeEach
   void setUp() {
-    Sale sale = new Sale();
-    sale.setId(1L);
-    sale.setPassword("password");
-    sale.setLast_name("last_name");
-    sale.setFirst_name("first_name");
-    sale.setRole(ApplicationUserRole.SALE_ADMIN);
+    Sale sale1 = new Sale();
+    sale1.setPassword("password");
+    sale1.setLast_name("last_name1");
+    sale1.setFirst_name("first_name1");
+    sale1.setRole(ApplicationUserRole.SALE_ADMIN);
 
     Company company1 = new Company();
+    company1.setSale(sale1);
     company1.setSize(10);
-    company1.setSale(sale);
-    company1.setSales_id(1L);
     company1.setName("company1");
     company1.setCity("guangzhou");
     company1.setSector("consumer");
     company1.setStateAbbr("guangdong");
 
-    sale.setCompanies(Set.of(company1));
-
     Company company2 = new Company();
+    company2.setSale(sale1);
     company2.setSize(20);
     company2.setName("company2");
     company2.setCity("guangzhou");
     company2.setSector("consumer");
     company2.setStateAbbr("guangdong");
 
-    saleRepo.save(sale);
+    Sale sale2 = new Sale();
+    sale2.setPassword("password");
+    sale2.setLast_name("last_name2");
+    sale2.setFirst_name("first_name2");
+    sale2.setRole(ApplicationUserRole.SALE_ADMIN);
+
+    Company company3 = new Company();
+    company3.setSale(sale2);
+    company3.setSize(10);
+    company3.setName("company3");
+    company3.setCity("guangzhou");
+    company3.setSector("sport");
+    company3.setStateAbbr("guangdong");
+
+    Company company4 = new Company();
+    company4.setSale(sale2);
+    company4.setSize(20);
+    company4.setName("company4");
+    company4.setCity("guangzhou");
+    company4.setSector("sport");
+    company4.setStateAbbr("guangdong");
+
+    sale1.setCompanies(Set.of(company1, company2));
+    sale2.setCompanies(Set.of(company3, company4));
+
+    saleRepo.saveAll(List.of(sale1, sale2));
   }
 
   @AfterEach
@@ -98,18 +120,18 @@ public class companyMapperTest implements WithAssertions {
     String query
   ) {
     List<Company> actual = mapper.getFilteredCompanies(
-      start,
-      take,
-      sort,
-      order,
-      sales_id,
-      minSize,
-      maxSize,
-      sector,
-      query
+      0,
+      2,
+      "id",
+      "desc",
+      1L,
+      0,
+      20,
+      "consumer",
+      null
     );
 
-    // actual.forEach(c -> System.out.println(c.toString()));
+    // System.out.println("******************** size: " + actual.size());
     assertThat(actual).isNotEmpty();
   }
 
@@ -145,38 +167,40 @@ public class companyMapperTest implements WithAssertions {
   private static Stream<Arguments> provideParamsForGetFilteredCompanies() {
     return Stream.of(
       // test start & take
-      Arguments.of(0, 2, "id", "desc", null, 0, 20, "consumer", null),
-      Arguments.of(1, 2, "id", "desc", null, 0, 20, "consumer", null),
+      Arguments.of(0, 2, "id", "desc", null, 0, 20, null, null),
+      Arguments.of(1, 2, "id", "desc", null, 0, 20, null, null),
       // test order & sort
-      Arguments.of(0, 2, "id", "desc", null, 0, 20, "consumer", null),
-      Arguments.of(0, 2, "id", "asc", null, 0, 20, "consumer", null),
-      Arguments.of(0, 2, "name", "asc", null, 0, 20, "consumer", null),
-      Arguments.of(0, 2, "name", "desc", null, 0, 20, "consumer", null),
-      // test sales_id
-      Arguments.of(0, 2, "id", "desc", null, 0, 20, "consumer", null),
-      Arguments.of(0, 2, "id", "desc", 1L, 0, 20, "consumer", null),
+      Arguments.of(0, 2, "id", "asc", null, 0, 20, null, null),
+      Arguments.of(0, 2, "name", "asc", null, 0, 20, null, null),
+      Arguments.of(0, 2, "name", "desc", null, 0, 20, null, null),
       // test minSize & maxSize
-      Arguments.of(0, 2, "id", "desc", null, null, 20, "consumer", null),
-      Arguments.of(0, 2, "id", "desc", null, 0, null, "consumer", null),
-      Arguments.of(0, 2, "id", "desc", null, 0, 10, "consumer", null),
+      Arguments.of(0, 2, "id", "desc", null, null, 20, null, null),
+      Arguments.of(0, 2, "id", "desc", null, 0, null, null, null),
+      Arguments.of(0, 2, "id", "desc", null, 0, 10, null, null),
+      Arguments.of(0, 2, "id", "desc", null, 10, 20, null, null),
+      Arguments.of(0, 2, "id", "desc", null, 20, 30, null, null),
+      // sector
+      Arguments.of(0, 2, "id", "desc", null, 0, 20, "sport", null),
       Arguments.of(0, 2, "id", "desc", null, 0, 20, "consumer", null),
-      Arguments.of(0, 2, "id", "desc", null, 10, 20, "consumer", null),
-      Arguments.of(0, 2, "id", "desc", null, 20, 30, "consumer", null),
+      // // sales_id
+      // Arguments.of(0, 2, "id", "desc", 1L, 0, 20, null, null),
+      // Arguments.of(0, 2, "id", "desc", 2L, 0, 20, null, null),
       // test query
-      Arguments.of(0, 2, "name", "desc", null, 0, 20, "consumer", "comp"), // company name
-      Arguments.of(0, 2, "name", "desc", null, 0, 20, "consumer", "company1"), // company full name
-      Arguments.of(0, 2, "name", "desc", null, 0, 20, "consumer", "guangz"), // city
-      Arguments.of(0, 2, "name", "desc", null, 0, 20, "consumer", "guangzhou"), // city full name
-      Arguments.of(0, 2, "name", "desc", null, 0, 20, "consumer", "consum"), // sector
-      Arguments.of(0, 2, "name", "desc", null, 0, 20, "consumer", "consumer"), // sector full name
-      Arguments.of(0, 2, "name", "desc", null, 0, 20, "consumer", "guangd"), // state abbriviation
-      Arguments.of(0, 2, "name", "desc", null, 0, 20, "consumer", "guangdong") // state abbriviation full name
+      Arguments.of(0, 2, "name", "desc", null, 0, 20, null, "comp"), // company name
+      Arguments.of(0, 2, "name", "desc", null, 0, 20, null, "company1"), // company full name
+      Arguments.of(0, 2, "name", "desc", null, 0, 20, null, "guangz"), // city
+      Arguments.of(0, 2, "name", "desc", null, 0, 20, null, "guangzhou"), // city full name
+      Arguments.of(0, 2, "name", "desc", null, 0, 20, null, "consum"), // sector
+      Arguments.of(0, 2, "name", "desc", null, 0, 20, null, "consumer"), // sector full name
+      Arguments.of(0, 2, "name", "desc", null, 0, 20, null, "guangd"), // state abbriviation
+      Arguments.of(0, 2, "name", "desc", null, 0, 20, null, "guangdong") // state abbriviation full name
     );
   }
 
   private static Stream<Arguments> testone() {
     return Stream.of(
-      Arguments.of(0, 2, "id", "desc", 1L, 0, 20, "consumer", null)
+      Arguments.of(0, 2, "id", "desc", 1L, 0, 20, null, null),
+      Arguments.of(0, 2, "id", "desc", 2L, 0, 20, null, null)
     );
   }
 
