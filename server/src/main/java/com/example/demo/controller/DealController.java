ede295fd3fd0624a4463181a0961c98c4eebc42d
Enfield-Li <1076462153@qq.com>
2022-07-20 14:11:00 +0800

    .

---
+++ server/src/main/java/com/example/demo/controller/DealController.java
@@ -0,0 +1,193 @@
+package com.example.demo.controller;
+
+import static com.example.demo.util.Constants.*;
+
+import com.example.demo.dto.UpdateDealDto;
+import com.example.demo.entity.Company;
+import com.example.demo.entity.Contact;
+import com.example.demo.entity.Deal;
+import com.example.demo.entity.Sale;
+import com.example.demo.exception.ItemNotFoundException;
+import com.example.demo.mapper.DealMapper;
+import com.example.demo.repository.DealRespository;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import javax.persistence.EntityManager;
+import javax.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Tag(name = "Deal")
+@RequestMapping(DEALS_ENDPOINT)
+class DealController {
+
+  private final DealRespository dealRepo;
+  private final EntityManager entityManager;
+  private final DealMapper dealMapper;
+
+  public DealController(
+    DealRespository dealRepo,
+    EntityManager entityManager,
+    DealMapper dealMapper
+  ) {
+    this.dealRepo = dealRepo;
+    this.entityManager = entityManager;
+    this.dealMapper = dealMapper;
+  }
+
+  @PostMapping("test")
+  public List<Deal> test() {
+    // return dealMapper.getCompanyDeals();
+    return null;
+  }
+
+  @PostMapping("bulk_insert")
+  public void saveAll(@RequestBody List<Deal> deals) {
+    deals.forEach(deal -> setRelationship(deal));
+    dealRepo.saveAll(deals);
+  }
+
+  @GetMapping
+  public ResponseEntity<List<Deal>> getAll(
+    @RequestParam(name = "_start") Integer start,
+    @RequestParam(name = "_end") Integer end,
+    @RequestParam(name = "_order") String order,
+    @RequestParam(name = "_sort") String sort,
+    @RequestParam(name = "q", required = false) String query,
+    @RequestParam(name = "type", required = false) String type,
+    @RequestParam(name = "sales_id", required = false) Long sales_id,
+    @RequestParam(name = "company_id", required = false) Long company_id,
+    @RequestParam(name = "stage_neq", required = false) String stage
+  ) {
+    Integer take = end - start;
+    if (sort.equals("index")) sort = "_index";
+    if (sort.equals("last_seen")) sort = "start_at";
+
+    List<Deal> companyDeals = dealMapper.getCompanyDeals(
+      start,
+      take,
+      sort,
+      order,
+      type,
+      query,
+      sales_id,
+      company_id,
+      stage
+    );
+
+    String dealCount = dealMapper.getDealCount(
+      type,
+      query,
+      sales_id,
+      company_id,
+      stage
+    );
+
+    return ResponseEntity
+      .ok()
+      .header("X-Total-Count", dealCount)
+      .body(processDeal(companyDeals));
+  }
+
+  @GetMapping(params = "id")
+  public ResponseEntity<List<Deal>> getDealList(
+    @RequestParam("id") List<Long> ids
+  ) {
+    List<Deal> deals = dealMapper.getDealsReference(ids);
+
+    return ResponseEntity.ok(deals);
+  }
+
+  @GetMapping("{id}")
+  public ResponseEntity<Deal> getById(@PathVariable("id") Long id) {
+    Deal deal = dealRepo
+      .findById(id)
+      .orElseThrow(() -> new ItemNotFoundException("Deal", id));
+
+    return ResponseEntity.ok(deal);
+  }
+
+  @PostMapping
+  public ResponseEntity<Deal> create(@RequestBody Deal deal) {
+    Deal savedDeal = dealRepo.save(setRelationship(deal));
+    return ResponseEntity.ok(savedDeal);
+  }
+
+  @PutMapping("{id}")
+  public ResponseEntity<Boolean> update(
+    @PathVariable("id") Long id,
+    @Valid @RequestBody UpdateDealDto item
+  ) {
+    Integer rowsAffected = dealMapper.updateDealStatus(id, item.getStage());
+    return ResponseEntity.ok(rowsAffected > 0);
+  }
+
+  @DeleteMapping("{id}")
+  public ResponseEntity<HttpStatus> delete(@PathVariable("id") Long id) {
+    dealRepo.deleteById(id);
+    return ResponseEntity.ok().build();
+  }
+
+  private Deal setRelationship(Deal deal) {
+    Sale sale = entityManager.getReference(Sale.class, deal.getSales_id());
+
+    Company company = entityManager.getReference(
+      Company.class,
+      deal.getCompany_id()
+    );
+
+    Set<Contact> contact_list = new HashSet<>();
+    deal
+      .getContact_list()
+      .forEach(
+        contactId -> {
+          Contact contact = entityManager.getReference(
+            Contact.class,
+            contactId
+          );
+          contact_list.add(contact);
+        }
+      );
+
+    deal.setSale(sale);
+    deal.setCompany(company);
+    deal.setContact_list(contact_list);
+
+    return deal;
+  }
+
+  private List<Deal> processDeal(List<Deal> deals) {
+    deals.forEach(
+      deal -> {
+        String contactIdsString = deal.getContactIdsString();
+
+        if (contactIdsString != null) {
+          String[] ids = contactIdsString.split(",");
+          List<String> idsString = Arrays.asList(ids);
+
+          List<Long> idsLong = new ArrayList<>();
+          idsString.forEach(id -> idsLong.add(Long.parseLong(id)));
+
+          deal.setContact_ids(idsLong);
+          deal.setContactIdsString(null);
+        }
+      }
+    );
+
+    return deals;
+  }
+}
---
+++ server/src/main/java/com/example/demo/controller/SaleController.java
@@ -0,0 +1,183 @@
+package com.example.demo.controller;
+
+import static com.example.demo.util.Constants.*;
+
+import com.example.demo.dto.LoginSaleDto;
+import com.example.demo.dto.RegisterSaleDto;
+import com.example.demo.dto.SaleRegisterResponseDto;
+import com.example.demo.dto.UpdateSaleDto;
+import com.example.demo.entity.Sale;
+import com.example.demo.exception.ItemNotFoundException;
+import com.example.demo.mapper.SaleMapper;
+import com.example.demo.repository.SaleRepository;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Tag(name = "Sale")
+@RequestMapping(SALES_ENDPOINT)
+class SaleController {
+
+  private static final String PENDING = "pending";
+  private static final String FORMAL = "formal";
+
+  private final SaleRepository saleRepo;
+  private final SaleMapper saleMapper;
+  private final PasswordEncoder passwordEncoder;
+
+  public SaleController(
+    SaleRepository saleRepo,
+    SaleMapper saleMapper,
+    PasswordEncoder passwordEncoder
+  ) {
+    this.saleRepo = saleRepo;
+    this.saleMapper = saleMapper;
+    this.passwordEncoder = passwordEncoder;
+  }
+
+  @PostMapping(TEST)
+  public String test() {
+    return "Have access";
+  }
+
+  @PostMapping(LOGIN)
+  public void login(@Valid @RequestBody LoginSaleDto dto) {}
+
+  @PostMapping(REGISTER)
+  public ResponseEntity<SaleRegisterResponseDto> register(
+    @Valid @RequestBody RegisterSaleDto dto,
+    HttpSession session
+  ) {
+    try {
+      String username = dto.getUsername();
+
+      String firstName = null;
+      String lastName = null;
+
+      boolean isFullName = username.contains(" ");
+
+      if (isFullName) {
+        String[] usernameArr = username.split(" ");
+        firstName = usernameArr[0];
+        lastName = usernameArr[1];
+      } else {
+        firstName = username;
+      }
+
+      Sale newSale = new Sale();
+      newSale.setStatus(PENDING);
+      newSale.setFirst_name(firstName);
+      newSale.setLast_name(lastName);
+      newSale.setPassword(passwordEncoder.encode(dto.getPassword()));
+      newSale.setRole(dto.getRole());
+
+      Sale savedSale = saleRepo.save(newSale);
+      session.setAttribute(ApplicationUserInSession, savedSale);
+
+      return ResponseEntity.ok(new SaleRegisterResponseDto(savedSale, null));
+    } catch (DataIntegrityViolationException err) {
+      return ResponseEntity
+        .badRequest()
+        .body(new SaleRegisterResponseDto(null, "Username already exist."));
+    }
+  }
+
+  @PostMapping(LOGOUT)
+  public void logout() {}
+
+  @GetMapping("me")
+  public Authentication me() {
+    return SecurityContextHolder.getContext().getAuthentication();
+  }
+
+  @GetMapping
+  public ResponseEntity<List<Sale>> getAll(
+    @RequestParam(name = "_start") Integer start,
+    @RequestParam(name = "_end") Integer end,
+    @RequestParam(name = "_order") String order,
+    @RequestParam(name = "_sort") String sort
+  ) {
+    Integer take = end - start;
+
+    List<Sale> sales = saleMapper.getAllSales(start, take, sort, order);
+    String saleCount = saleMapper.getSaleCount();
+
+    return ResponseEntity.ok().header("X-Total-Count", saleCount).body(sales);
+  }
+
+  @GetMapping(params = "id")
+  public ResponseEntity<List<Sale>> getListById(
+    @RequestParam(name = "id") Long id
+  ) {
+    Sale sales = saleRepo
+      .findById(id)
+      .orElseThrow(() -> new ItemNotFoundException("Sales", id));
+
+    return ResponseEntity.ok(Arrays.asList(sales));
+  }
+
+  @Transactional
+  @PutMapping(UPDATE_ROLE)
+  public ResponseEntity<Sale> update(@Valid @RequestBody UpdateSaleDto dto) {
+    Optional<Sale> sale = null;
+
+    if (dto.getId() != null) {
+      sale = saleRepo.findById(dto.getId());
+    } else if (dto.getUsername() != null) {
+      sale = saleRepo.findByFullName(dto.getUsername());
+    }
+
+    sale.orElseThrow(
+      () -> new ItemNotFoundException("Sales person", dto.getUsername())
+    );
+
+    sale.get().setRole(dto.getRole());
+    sale.get().setStatus(FORMAL);
+
+    return ResponseEntity.ok(sale.get());
+  }
+
+  @DeleteMapping("{id}")
+  public ResponseEntity<HttpStatus> delete(@PathVariable("id") Long id) {
+    saleRepo.deleteById(id);
+    return ResponseEntity.ok().build();
+  }
+
+  @PostMapping("bulk_insert")
+  public void saveAll(@RequestBody List<Sale> sales) {
+    saleRepo.saveAll(sales);
+  }
+
+  @Transactional
+  @GetMapping("set_password")
+  public void setPassword() {
+    List<Sale> sales = saleRepo.findAll();
+
+    sales.forEach(
+      sale -> {
+        String fullName = sale.getFirst_name() + " " + sale.getLast_name();
+        sale.setPassword(passwordEncoder.encode(fullName));
+      }
+    );
+  }
+}
